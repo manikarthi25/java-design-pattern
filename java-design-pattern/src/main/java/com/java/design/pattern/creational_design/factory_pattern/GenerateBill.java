2b66cdf253b0be681a2955525707e0add02ca54d
manikarthi25 <manikarthi25@gmail.com>
2022-02-13 18:38:49 +0530

    factory design pattern

--- java-design-pattern/src/main/java/com/java/design/pattern/creational_design/factory_pattern/GenerateBill.java
+++ java-design-pattern/src/main/java/com/java/design/pattern/creational_design/factory_pattern/GenerateBill.java
@@ -1,24 +1,18 @@
 package com.java.design.pattern.creational_design.factory_pattern;
 
-/*
-Generate Bill by using the GetPlanFactory to get the object of concrete classes 
-by passing an information such as type of plan DOMESTICPLAN or COMMERCIALPLAN or 
-INSTITUTIONALPLAN
-*/
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 class GenerateBill {
 	public static void main(String args[]) throws IOException {
 		GetPlanFactory planFactory = new GetPlanFactory();
 
 		System.out.print("Enter the name of plan for which the bill will be generated: ");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);
 
-		String planName = br.readLine();
+		String planName = sc.nextLine();
 		System.out.print("Enter the number of units for bill will be calculated: ");
-		int units = Integer.parseInt(br.readLine());
+		int units = sc.nextInt();
 
 		Plan p = planFactory.getPlan(planName);
 		// call getRate() method and calculateBill()method of DomesticPaln.
@@ -29,8 +23,8 @@ class GenerateBill {
 	}
 }
 
-/* Output
-Enter the name of plan for which the bill will be generated: CommercialPlan
-Enter the number of units for bill will be calculated: 2
-Bill amount for CommercialPlan of  2 units is: 15.0
-*/
+/*
+ * Output Enter the name of plan for which the bill will be generated: CommercialPlan 
+ * Enter the number of units for bill will be calculated: 2
+ * Bill amount for CommercialPlan of 2 units is: 15.0
+ */
